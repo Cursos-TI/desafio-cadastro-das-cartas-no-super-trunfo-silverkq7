b5ce53d8e1317591f8b08329568da8778a86f789
silverkq7 <vinico365@gmail.com>
2025-02-17 19:25:59 +0000

    Novo Commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,9 +3,27 @@
 
 int main() {
     printf("Carta Super trunfo\n");
-    printf("Novo commit\n")
-char Estado= "Rio de janeiro\n";
-char codigo da carta [A01] 
+char Estado= "Rio de janeiro";
+char codigo_da_carta [20]= "A01";
+char Nome_da_Cidade [30]= "Saquarema";
+int Populaçao= 140000;
+float Area_em_Km= 20000 "km";
+float PIB= 220000;
+int Numero_de_Pontos_Turistico= "três";
+
+char Estado= "Rio de janeiro";
+char codigo_da_carta [20]= "A02";
+char Nome_da_Cidade [30]= "Bangu";
+int Populaçao= 15000;
+float Area_em_Km= 3000 "km";
+float PIB= 20000;
+int Numero_de_Pontos_Turistico= "Nenhum";
+
+
+
+
+
+
 
 return 0;
 }
